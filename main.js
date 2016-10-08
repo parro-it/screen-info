ec6a36b516948ff1521916c12f343ef02e62ec12
Andrea Parodi <andrea@parro.it>
2016-10-08 09:54:12 +0200

    templates

--- main.js
+++
@@ -1,9 +0,0 @@
-export default function * spread(source) {
-	for (const item of source) {
-		if (typeof item[Symbol.iterator] === 'function') {
-			yield * item;
-		} else {
-			yield item;
-		}
-	}
-}
--- test.js
+++
@@ -1,8 +0,0 @@
-import 'babel-register';
-import test from 'ava';
-import spread from './main';
-
-test('command with one argument', t => {
-	const result = Array.from(spread([[1, 2, 3], 42, 43]));
-	t.deepEqual(result, [1, 2, 3, 42, 43]);
-});
