2d7331d1791f6e87d4f2352becd0e6ad4a408306
Andrea Parodi <andrea@parro.it>
2016-12-18 15:37:09 +0100

    win32

--- src/windows-screen.cc
+++ src/windows-screen.cc
@@ -1,13 +1,22 @@
-#include <windows.h>
+#include <wtypes.h>
 #include "nbind/nbind.h"
 #include "screen-size.h"
 
+using namespace std;
 
 struct ScreenInfo {
 	static ScreenSize mainDisplaySize() {
+		 RECT desktop;
+		// Get a handle to the desktop window
+		const HWND hDesktop = GetDesktopWindow();
+		// Get the size of screen to the variable desktop
+		GetWindowRect(hDesktop, &desktop);
+		// The top left corner will have coordinates (0,0)
+		// and the bottom right corner will have coordinates
+		// (horizontal, vertical)
 		return ScreenSize(
-			(size_t)GetSystemMetrics(SM_CXSCREEN),
-			(size_t)GetSystemMetrics(SM_CYSCREEN)
+			(size_t) desktop.right,
+			(size_t) desktop.bottom
 		);
 	}
 };
